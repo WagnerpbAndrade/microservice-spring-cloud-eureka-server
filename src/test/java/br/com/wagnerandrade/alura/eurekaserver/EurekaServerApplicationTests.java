791107cabb7172b3a69b6f96ee8bd99ed707368f
Wagner Andrade <wagnerpbandrade@gmail.com>
2021-02-16 17:13:40 -0300

    Criando servidor Eureka

---
+++ src/test/java/br/com/wagnerandrade/alura/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wagnerandrade.alura.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
